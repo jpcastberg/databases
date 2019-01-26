91df49c996f9e5b06660fe02b7d455167f149340
jpcastberg <jpcastberg@gmail.com>
2019-01-25 20:53:38 -0800

    Implement schema with users and messages tables

--- server/schema.sql
+++ server/schema.sql
@@ -1,15 +1,28 @@
-CREATE DATABASE chat;
 
+
+-- CREATE DATABASE chat;
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
 
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE users (
+  id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  username VARCHAR(32),
+  PRIMARY KEY (id)
+);
 
-
+CREATE TABLE messages (
+  id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  user SMALLINT UNSIGNED NOT NULL,
+  text VARCHAR(150),
+  roomname VARCHAR(32),
+  time_created TIMESTAMP,
+  PRIMARY KEY (id),
+  -- CONSTRAINT FOREIGN KEY user references users id
+  FOREIGN KEY (user)
+    REFERENCES users(id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
